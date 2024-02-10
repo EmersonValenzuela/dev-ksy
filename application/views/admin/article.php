dac1f549b6837916ac2a8ee806c44273cf0c6f34
Emerson Valenzuela <52836406+EmersonValenzuela@users.noreply.github.com>
2024-02-09 22:42:34 -0500

    ✅  bug rreport

--- application/views/admin/article.php
+++ application/views/admin/article.php
@@ -9,11 +9,10 @@
                               </div>
                               <div class="col-6">
                                   <ol class="breadcrumb">
-                                      <li class="breadcrumb-item"><a href="index.html">
+                                      <li class="breadcrumb-item"><a href="<?= base_url() ?>">
                                               <svg class="stroke-icon">
                                                   <use href="<?= base_url() ?>assets/svg/icon-sprite.svg#stroke-home"></use>
                                               </svg></a></li>
-                                      <li class="breadcrumb-item">Dashboard</li>
                                       <li class="breadcrumb-item active">Articulos</li>
                                   </ol>
                               </div>
--- application/views/admin/reports.php
+++ application/views/admin/reports.php
@@ -3,11 +3,11 @@
         <div class="page-title">
             <div class="row">
                 <div class="col-6">
-                    <h4>Reportes Control Temperatura</h4>
+                    <h4>Reportes Ventas</h4>
                 </div>
                 <div class="col-6">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="Dashboard-Temperatura">
+                        <li class="breadcrumb-item"><a href="<?= base_url() ?>">
                                 <svg class="stroke-icon">
                                     <use href="<?= base_url() ?>assets/svg/icon-sprite.svg#stroke-home"></use>
                                 </svg></a></li>
